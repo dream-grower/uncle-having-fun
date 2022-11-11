fa330663e89ade423d4826017ccfe1c8f4faabaf
recvec <mr.recvec@gmail.com>
2022-11-11 15:26:10 +0200

    init

---
+++ test/neurographic_dart_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:neurographic_dart_server/neurographic_dart_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
